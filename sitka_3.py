118e720839088420355397d230db63f6b697b9fb
Chris Keller <ckeller191@gmail.com>
2021-02-24 13:43:16 -0600

    done with 2/22 lecture

--- sitka_3.py
+++ sitka_3.py
@@ -1,7 +1,14 @@
+#changing file to include data for year of all 2018
+#change title to Daily low and high temperatures - 2018
+#extract low temps from file and add to chart
+#shade in area between high and low
+
+
+
 import csv
 from datetime import datetime
 
-open_file = open("sitka_weather_07-2018_simple.csv", "r")
+open_file = open("sitka_weather_2018_simple.csv", "r")
 
 csv_file = csv.reader(open_file, delimiter= ",")
 
@@ -12,11 +19,13 @@ for index, column_header in enumerate(header_row):
 
 highs = []
 dates = []
+lows = []
 
 
 
 for row in csv_file:
     highs.append(int(row[5]))
+    lows.append(int(row[6]))
     converted_date = datetime.strptime(row[2], "%Y-%m-%d")
     dates.append(converted_date)
 
@@ -27,13 +36,23 @@ import matplotlib.pyplot as plt
 fig = plt.figure()
 
 plt.plot(dates, highs, c="red")
+plt.plot(dates, lows, c="blue")
 
 fig.autofmt_xdate()
 
-plt.title("Daily high temperatures, July 2018", fontsize=16)
+plt.fill_between(dates, highs, lows, facecolor='blue', alpha=0.1)
+
+plt.title("Daily high and low temperatures - 2018", fontsize=16)
 plt.xlabel('', fontsize=12)
 plt.ylabel("Temperature (F)", fontsize=12)
 plt.tick_params(axis="both", labelsize=12)
 
+
+plt.show()
+
+fig2, a = plt.subplots(2)
+
+a[0].plot(dates, highs, c='red')
+a[1].plot(dates, lows, c='blue')
 
 plt.show()
